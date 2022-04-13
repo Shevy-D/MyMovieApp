43bfbc5e2ab00feb879d24191a8c14bb1887b1ab
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-04-13 18:13:54 +0500

    Added RegistrationActivity

--- app/src/main/java/com/shevy/mymovieapp/MainActivity.kt
+++ app/src/main/java/com/shevy/mymovieapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.shevy.mymovieapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_click_me = findViewById<Button>(R.id.main_activity_register_button)
+
+        btn_click_me.setOnClickListener{
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "You click me", Toast.LENGTH_SHORT).show()
+        }
     }
+
+/*    fun clickBut(view: View) {
+        Toast.makeText(this, "You click me", Toast.LENGTH_SHORT).show()
+    }*/
+
+
 }
---
+++ app/src/main/java/com/shevy/mymovieapp/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.shevy.mymovieapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
