f2f8abaa4979bbb34e9e8181d4b71622ffbc5c60
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-04-14 11:29:07 +0500

    Added registerForActivityResult

--- app/src/main/java/com/shevy/mymovieapp/MainActivity.kt
+++ app/src/main/java/com/shevy/mymovieapp/MainActivity.kt
@@ -1,26 +1,45 @@
 package com.shevy.mymovieapp
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
-import com.squareup.picasso.Picasso
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+
+/*    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { resultCallback ->
+
+        this.onSignInResult(resultCallback)
+    }*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        database = Firebase.database.reference
+        database.child("user2").child("userId").setValue("user")
+        Log.d("Database Main Activity", "Database2 = $database")
 
         val btn_click_me = findViewById<Button>(R.id.main_activity_register_button)
 
         btn_click_me.setOnClickListener{
-            val intent = Intent(this, RegistrationActivity::class.java)
+            val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
             startActivity(intent)
-            Toast.makeText(this, "You click me", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "You click me", Toast.LENGTH_SHORT).show()
         }
     }
+
+/*    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        //
+    }*/
 
 /*    fun clickBut(view: View) {
         Toast.makeText(this, "You click me", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/shevy/mymovieapp/RegistrationActivity.kt
+++ app/src/main/java/com/shevy/mymovieapp/RegistrationActivity.kt
@@ -1,11 +1,47 @@
 package com.shevy.mymovieapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val provider = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(provider)
+            .build()
+        signInLauncher.launch(signInIntent)
+        Log.d("Database Reg", "signInLauncher = $signInLauncher, signInIntent = $signInIntent")
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 }
